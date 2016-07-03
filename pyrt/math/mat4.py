3e7fcc5db02677db8c55406b23b13bb84f9c9953
Martin Christen <martin.christen@fhnw.ch>
2016-07-03 20:54:44 +0200

    added prototypes of mat4 and vec4

---
+++ pyrt/math/mat4.py
@@ -0,0 +1,10 @@
+import math
+from .constants import *
+
+
+class Mat4:
+    """
+    Class representing a 4x4 Matrix
+    """
+    def __init__(self):
+        pass
---
+++ pyrt/math/vec4.py
@@ -0,0 +1,12 @@
+import math
+from .constants import *
+
+
+class Vec4:
+    """Class representing a 3D-Vector
+    params:
+       x,y,z,w Component of vector (float or int)
+    """
+    def __init__(self, x=0.0, y=0.0, z=0.0, w=0.0):
+        pass
+
