538e0392b05b36eaaf7f5014b9c9491eb82c06a3
Martin Christen <martin.christen@fhnw.ch>
2016-07-04 10:33:56 +0200

    class is more pythonic
    * Improved constructor (takes list, tuple or keyword args)
    * Data access improved

--- pyrt/math/vec3.py
+++ pyrt/math/vec3.py
@@ -2,19 +2,65 @@ import math
 from .constants import *
 
 class Vec3:
-    """Class representing a 3D-Vector
-    params:
-       x,y,z Component of vector (float or int)
+    """Class representing a 3D-Vector. Values are always stored as float.
     """
-    def __init__(self, x=0.0, y=0.0, z=0.0):
+    def __init__(self, *args, **kwargs):
         """
-        :param x: x-component 
-        :param y: y-component
-        :param z: z-component
+        Vec3 can be constructed the following way:
+
+        v1 = Vec3(x,y,z)    # 3 integers or floats
+        v2 = Vec3([x,y,z])  # from a list of 3 integers or floats
+        v3 = Vec3((x,y,z))  # from a tuple of 3 integers or floats
+        v4 = Vec3(x)
+
+        # using named arguments
+        v5 = Vec3(z=1)
+        v6 = Vec3(x=1, y=2, z=3)
+        v7 = Vec3(y=4, x=2)
+
+        # using nothing (Null-Vector)
+        v7 = Vec3()
+
         """
-        self.x = float(x)
-        self.y = float(y)
-        self.z = float(z)
+
+        if len(kwargs) == 0:
+            if len(args) == 3:
+                    self.x = float(args[0])
+                    self.y = float(args[1])
+                    self.z = float(args[2])
+            elif len(args) == 1:
+                if type(args) == tuple or type(args) == list:
+                    self.x = float(args[0][0])
+                    self.y = float(args[0][1])
+                    self.z = float(args[0][2])
+                else:
+                    raise ValueError("Sequencial definition of Vec3 must be list or tuple!")
+
+            elif len(args) == 0:
+                self.x = self.y = self.z = 0.0
+
+            else:
+                raise ValueError("Wrong number of parameters in Vec3")
+        else:
+
+            if len(args) > 0:
+                raise ValueError("Use keyword arguments OR normal arguments, both is not permitted")
+
+            if "x" in kwargs:
+                self.x = float(kwargs["x"])
+            else:
+                self.x = 0
+
+            if "y" in kwargs:
+                self.y = float(kwargs["y"])
+            else:
+                self.y = 0
+
+            if "z" in kwargs:
+                self.z = float(kwargs["z"])
+            else:
+                self.z = 0
+
 
     def __str__(self):
         """Convert vector to string
@@ -69,6 +115,26 @@ class Vec3:
 
     def __neg__(self):
         return Vec3(-self.x, -self.y, -self.z)
+
+    def __getitem__(self, key):
+        if key == 0:
+            return self.x
+        elif key == 1:
+            return self.y
+        elif key == 2:
+            return self.z
+        else:
+            raise IndexError("Vec3 has 3 components: [0] [1] and [2]")
+
+    def __setitem__(self, key, value):
+        if key == 0:
+             self.x = float(value)
+        elif key == 1:
+            self.y = float(value)
+        elif key == 2:
+            self.z = float(value)
+        else:
+            raise IndexError("Vec3 has 3 components: [0] [1] and [2]")
 
     def isZero(self):
         return math.fabs(self.x) < G_EPSILON and math.fabs(self.y) < G_EPSILON and math.fabs(self.z) < G_EPSILON
