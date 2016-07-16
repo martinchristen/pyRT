d33a734b3bf9d408c4acd55438e6669b9909bf20
Martin Christen <martin.christen@fhnw.ch>
2016-07-16 11:28:22 +0200

    added Zero/Translation

--- pyrt/math/matops.py
+++ pyrt/math/matops.py
@@ -8,8 +8,39 @@ from .mat4 import *
 
 
 def CreateIdentity4():
+    """
+    Create a 4x4 identity matrix
+    """
     return Mat4((1,0,0,0,
                  0,1,0,0,
                  0,0,1,0,
                  0,0,0,1))
+
+
+def CreateZero4():
+    """
+    Create a 4x4 zero matrix
+    """
+    return Mat4()
+
+
+def CreateTranslation4(x,y,z,w=1):
+    """
+    Create a 4x4 translation matrix
+    """
+    return Mat4((1, 0, 0, x,
+                 0, 1, 0, y,
+                 0, 0, 1, z,
+                 0, 0, 0, w))
+
+
+def CreateScale4(x,y,z,w=1):
+    """
+    Create a 4x4 scale matrix
+    """
+    return Mat4((x, 0, 0, 0,
+                 0, y, 0, 0,
+                 0, 0, z, 0,
+                 0, 0, 0, w))
+
 
