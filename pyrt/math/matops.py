4ccf7af6504062c8e80334c8c8d8dda6f5c930ac
Martin Christen <martin.christen@fhnw.ch>
2016-07-17 11:23:32 +0200

    added rotation matrices (4x4)

--- pyrt/math/matops.py
+++ pyrt/math/matops.py
@@ -7,6 +7,14 @@ from .vec4 import *
 from .mat4 import *
 
 
+def deg2rad(v):
+    return v * math.pi / 180.
+
+
+def rad2deg(v):
+    return 180. * v / math.pi
+
+
 def CreateIdentity4():
     """
     Create a 4x4 identity matrix
@@ -42,5 +50,40 @@ def CreateScale4(x,y,z,w=1):
                  0, y, 0, 0,
                  0, 0, z, 0,
                  0, 0, 0, w))
+
+
+def CreateRotationX4(angle=0):
+    """
+    Create a rotation matrix
+    :param angle: rotation about x-axis (radiant)
+    :return: rotation matrix
+    """
+    return Mat4((1, 0, 0, 0,
+                 0, math.cos(angle), math.sin(angle), 0,
+                 0, -math.sin(angle), math.cos(angle), 0,
+                 0, 0, 0, 1))
+
+
+def CreateRotationY4(angle=0):
+    """
+    Create a rotation matrix
+    :param angle: rotation about y-axis (radiant)
+    :return: rotation matrix
+    """
+    return Mat4((math.cos(angle), 0, -math.sin(angle), 0,
+                 0, 1, 0, 0,
+                 math.sin(angle), 0, math.cos(angle), 0,
+                 0, 0, 0, 1))
+
+def CreateRotationZ4(angle=0):
+    """
+    Create a rotation matrix
+    :param angle: rotation about z-axis (radiant)
+    :return: rotation matrix
+    """
+    return Mat4((math.cos(angle), math.sin(angle), 0, 0,
+                 -math.sin(angle), math.cos(angle), 0, 0,
+                 0, 0, 1, 0,
+                 0, 0, 0, 1))
 
 
