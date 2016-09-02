247f6fb7a3cd4cc64acda9fc33358808c447d2d0
Martin Christen <martin.christen@fhnw.ch>
2016-09-02 11:59:59 +0200

    example OrthographicCamera (for tests)

--- pyrt/camera/orthographiccamera.py
+++ pyrt/camera/orthographiccamera.py
@@ -1,9 +1,18 @@
+from ..math import *
+
 
 class OrthographicCamera:
-    def __init__(self, width=640, height=480):
+    def __init__(self, width=512, height=512):
         """
         :param width: horizontal resolution of the output image
         :param height: vertical resolution of the output image
         """
+        self.width = width
+        self.height = height
+        self.direction = Vec3(0.0,0.0,-1.0)
 
         print("[init] Orthographic Camera")
+
+    def primaryRay(self, x, y):
+        r = Ray(Vec3(x,y,0), self.direction)
+        return r
