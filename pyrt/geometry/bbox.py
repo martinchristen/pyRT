12d97a9782a771ddcef59ce75c557f17860b8844
Martin Christen <martin.christen@fhnw.ch>
2016-10-09 14:04:37 +0200

    added ray box intersection

--- pyrt/geometry/bbox.py
+++ pyrt/geometry/bbox.py
@@ -22,6 +22,26 @@ class BBox(Shape):
         self.max = vmax
         self.extent = self.max - self.min
 
+    def __setitem__(self, key, value):
+        assert type(value) is Vec3, "value is not a Vec3"
+
+        if key == 0:
+            self.min = value
+        elif key == 1:
+            self.max = value
+        else:
+            raise IndexError("Can only set index 0 (min) and index 1 (max)")
+
+    def __getitem__(self, key):
+        if key == 0:
+            return self.min
+        elif key == 1:
+            return self.max
+        else:
+            raise IndexError("Can only get index 0 (min) and index 1 (max)")
+
+        pass
+
     def maxComponent(self):
         """
         Return the max component of bounding box
@@ -45,7 +65,7 @@ class BBox(Shape):
         self.max = max3(self.max, pt)
         self.extent = self.max - self.min
 
-    def expand(self, bbox: BBox):
+    def expand(self, bbox):
         """
         Expands the bounding box by another box
 
@@ -56,13 +76,39 @@ class BBox(Shape):
         self.extent = self.max - self.min
 
     def hit(self, ray: Ray, hitrecord: HitRecord) -> bool:
-        return False # TODO: Ray Box intersection
+        tmin = (self[ray.sign[0]].x - ray.start.x) * ray.invdir.x
+        tmax = (self[1 - ray.sign[0]].x - ray.start.x) * ray.invdir.x
+        tymin = (self[ray.sign[1]].y - ray.start.y) * ray.invdir.y
+        tymax = (self[1 - ray.sign[1]].y - ray.start.y) * ray.invdir.y
+        tzmin = (self[ray.sign[2]].z - ray.start.z) * ray.invdir.z
+        tzmax = (self[1 - ray.sign[2]].z - ray.start.z) * ray.invdir.z
+        tmin = max(max(tmin, tymin), tzmin)
+        tmax = min(min(tmax, tymax), tzmax)
+        tmin = (self[ray.sign[0]].x - ray.start.x) * ray.invdir.x
+
+        if tmin > tmax:
+            return False
+
+        hitrecord.t = tmin
+        hitrecord.point = ray.start + ray.direction * tmin
+        return True
 
     def hitShadow(self, ray: Ray) -> bool:
-        return False # TODO: Ray Box intersection
+        tmin = (self[ray.sign[0]].x - ray.start.x) * ray.invdir.x
+        tmax = (self[1 - ray.sign[0]].x - ray.start.x) * ray.invdir.x
+        tymin = (self[ray.sign[1]].y - ray.start.y) * ray.invdir.y
+        tymax = (self[1 - ray.sign[1]].y - ray.start.y) * ray.invdir.y
+        tzmin = (self[ray.sign[2]].z - ray.start.z) * ray.invdir.z
+        tzmax = (self[1 - ray.sign[2]].z - ray.start.z) * ray.invdir.z
+        tmin = max(max(tmin, tymin), tzmin)
+        tmax = min(min(tmax, tymax), tzmax)
+        tmin = (self[ray.sign[0]].x - ray.start.x) * ray.invdir.x
+
+        return tmin > tmax
+
 
     def surfaceArea(self) -> float:
         return 2. * (self.extent.x * self.extent.z +
-                     self.extent.x * self.extent.y +
-                     self.extent.y * self.extent.z)
+                    self.extent.x * self.extent.y +
+                    self.extent.y * self.extent.z)
 
