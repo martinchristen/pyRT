75d1a3080b6118312a0d2c2ef79d2576da0b0526
Martin Christen <martin.christen@fhnw.ch>
2016-12-28 09:00:40 +0100

    fixed a few issues with odd obj files

--- pyrt/geometry/importer/obj.py
+++ pyrt/geometry/importer/obj.py
@@ -17,12 +17,14 @@ from ..triangle import *
 from ..trianglemesh import *
 
 import os.path
+import uuid
+
 
 
 #-----------------------------------------------------------------------------------------------------------------------
+# Parse Material file
 def _mat_parse(materialfile, state):
     matfile = open(materialfile, "r")
-
 
     curmtl = ""
     materials = state["materials"]
@@ -40,32 +42,58 @@ def _mat_parse(materialfile, state):
                 print("found material: " + curmtl)
         elif line[0:2] == "Ns":
             m = line.split(" ")
-            materials[curmtl]["Ns"] = m[1]
+            materials[curmtl]["Ns"] = float(m[1])
         elif line[0:2] == "Ni":
             m = line.split(" ")
-            materials[curmtl]["Ni"] = m[1]
+            materials[curmtl]["Ni"] = float(m[1])
         elif line[0:2] == "Tr":
             m = line.split(" ")
-            materials[curmtl]["Tr"] = m[1]
+            materials[curmtl]["Tr"] = float(m[1])
+        elif line[0:2] == "d":
+            m = line.split(" ")
+            materials[curmtl]["Tr"] = 1. - float(m[1])
         elif line[0:2] == "Ka":
             m = line.split(" ")
-            materials[curmtl]["Ka"] = m[1]
+            materials[curmtl]["Ka"] = float(m[1])
         elif line[0:2] == "Kd":
             m = line.split(" ")
-            materials[curmtl]["Kd"] = m[1]
+            materials[curmtl]["Kd"] = float(m[1])
         elif line[0:2] == "Ks":
             m = line.split(" ")
-            materials[curmtl]["Ks"] = m[1]
+            materials[curmtl]["Ks"] = float(m[1])
         elif line[0:2] == "Ke":
             m = line.split(" ")
-            materials[curmtl]["Ke"] = m[1]
+            materials[curmtl]["Ke"] = float(m[1])
+        elif line[0:6] == "map_Ka":
+            m = line.split(" ")
+            materials[curmtl]["map_Ka"] = state["file"][1] + m[1]
         elif line[0:6] == "map_Kd":
             m = line.split(" ")
             materials[curmtl]["map_Kd"] = state["file"][1] + m[1]
+        elif line[0:6] == "map_Ks":
+            m = line.split(" ")
+            materials[curmtl]["map_Ks"] = state["file"][1] + m[1]
+        elif line[0:8] == "map_bump" or line[0:4] == "bump":
+            m = line.split(" ")
+            materials[curmtl]["map_bump"] = state["file"][1] + m[1]
+        elif line[0:4] == "disp":
+            m = line.split(" ")
+            materials[curmtl]["map_disp"] = state["file"][1] + m[1]
         elif line[0:5] == "illum":
             m = line.split(" ")
-            materials[curmtl]["illum"] = m[1]
-
+            materials[curmtl]["illum"] = int(m[1])
+            # Illum:
+            #  0: Color on and Ambient off
+            #  1: Color on and Ambient on
+            #  2: Highlight on
+            #  3: Reflection on and Ray trace on
+            #  4: Transparency: Glass on, Reflection: Ray trace on
+            #  5: Reflection: Fresnel on and Ray trace on
+            #  6: Transparency: Refraction on, Reflection: Fresnel off and Ray trace on
+            #  7: Transparency: Refraction on, Reflection: Fresnel on and Ray trace on
+            #  8: Reflection on and Ray trace off
+            #  9: Transparency: Glass on, Reflection: Ray trace off
+            # 10: Casts shadows onto invisible surfaces
 
 
     matfile.close()
@@ -73,6 +101,7 @@ def _mat_parse(materialfile, state):
 
 #-----------------------------------------------------------------------------------------------------------------------
 def _obj_parse(line, state):
+    curgroup = str(uuid.uuid4())[0:8]
     if line[0:6] == "mtllib":
         materialfile = line.split(" ")
         if len(materialfile) == 2:
@@ -81,20 +110,34 @@ def _obj_parse(line, state):
             _mat_parse(materialfile, state)
 
     elif line[0:2] == "v ":
-        v = line.split(" ")
-        state["raw_vertices"].append([float(v[1]), float(v[2]), float(v[3])])
+        v = line[1:len(line)].strip()
+        v = v.split(" ")
+        state["raw_vertices"].append([float(v[0]), float(v[1]), float(v[2])])
 
     elif line[0:2] == "vt":
-        vt = line.split(" ")
-        state["raw_texcoords"].append([float(vt[1]), float(vt[2])])
+        vt = line[2:len(line)].strip()
+        vt = vt.split(" ")
+        state["raw_texcoords"].append([float(vt[0]), float(vt[1])])
 
     elif line[0:2] == "vn":
-        vn = line.split(" ")
-        state["raw_normals"].append([float(vn[1]), float(vn[2]), float(vn[3])])
+        vn = line[2:len(line)].strip()
+        vn = vn.split(" ")
+        state["raw_normals"].append([float(vn[0]), float(vn[1]), float(vn[2])])
+
+    elif line[0:1] == "g":
+        g = line[1:len(line)].strip()
+        curgroup = g
+
+    elif line[0:1] == "f":
+        f = line[1:len(line)].strip()
+        f = f.split(" ")
+
+        #if len(f) != 3:
+        #    print("ERROR: Currently only triangulated objects are supported")
 
 
     #-----------------------------------------------------------------------------------------------------------------------
-def loadObj(filename: str) -> TriangleMesh:
+def loadObj(filename: str, debug: bool = False) -> TriangleMesh:
     parsestate = {}
 
     path, name = os.path.split(filename)
@@ -106,8 +149,8 @@ def loadObj(filename: str) -> TriangleMesh:
     parsestate["raw_texcoords"] = []
     parsestate["file"] = [name, path]
 
-
-    print("processing: " + name + " in directory " + path)
+    if debug:
+        print("processing: " + name + " in directory " + path)
 
     file = open(filename, "r")
     for line in file:
@@ -127,11 +170,12 @@ def loadObj(filename: str) -> TriangleMesh:
     # del parsestate["raw_texcoords"]
 
 
-    import pprint
-    print("PARSED OUTPUT:")
+    if debug:
+        import pprint
+        print("PARSED OUTPUT:")
 
-    pp = pprint.PrettyPrinter(depth=6)
-    pp.pprint(parsestate)
+        pp = pprint.PrettyPrinter(depth=6)
+        pp.pprint(parsestate)
 
 
 #-----------------------------------------------------------------------------------------------------------------------
