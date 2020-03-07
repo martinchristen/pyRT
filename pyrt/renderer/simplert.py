728f7e2a97de5e346618b6666a5d8b4fb1eecf78
DunaevaOlga <ksan_al@mail.ru>
2020-03-07 15:49:46 +0300

    Spot light processing added

--- pyrt/renderer/simplert.py
+++ pyrt/renderer/simplert.py
@@ -50,17 +50,30 @@ class SimpleRT(Renderer):
 
     def _shadow(self, scene: Scene, hitrecord: HitRecord) -> tuple:
         # is hitpoint in shadow ?
-        fs = 1.0
+        fs = 0.1
         local_num_shadow_rays = 0
         numshadow = 0
         for light in scene.lights:
             shadowray = Ray(hitrecord.point, light.position - hitrecord.point)
+            if light.name == "SpotLight":
+                radians = np.arccos(light.direction.dot(-shadowray.direction) / (light.direction.length() * shadowray.direction.length()))
+                if(np.degrees(radians) > light.angle):
+                    continue
+                else:
+                    fs += (1 - np.degrees(radians) / (1.5 * light.angle)) / len(scene.lights)
+            else:
+                fs += 1 / len(scene.lights)
+
             local_num_shadow_rays += 1
             for testelement in scene.nodes:
                 if testelement != hitrecord.obj:  # avoid self-intersection
                     if testelement.hitShadow(shadowray):
                         numshadow += 1
                         break
+
+
+        fs *= 0.6 * (len(scene.lights) + 1)
+        fs = np.clip(fs, 0, 1)
         for i in range(numshadow):
             fs /= 4.
 
